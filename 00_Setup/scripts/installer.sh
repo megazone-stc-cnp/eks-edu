3b70b7720a340874992130f57acf107d89ce101d
김종현 <yulisys@megazone.com>
2025-03-26 18:10:15 +0900

    bash-completion 처리 추가

--- 00_Setup/scripts/installer.sh
+++ 00_Setup/scripts/installer.sh
@@ -138,13 +138,6 @@ download "https://github.com/mikefarah/yq/releases/download/v${yq_version}/yq_li
 chmod +x ./yq
 mv ./yq /usr/local/bin
 
-# flux
-download "https://github.com/fluxcd/flux2/releases/download/v${flux_version}/flux_${flux_version}_linux_${arch_name}.tar.gz" "flux.tar.gz"
-tar zxf flux.tar.gz
-chmod +x flux
-mv ./flux /usr/local/bin
-rm -rf flux.tar.gz
-
 # terraform
 download "https://releases.hashicorp.com/terraform/${terraform_version}/terraform_${terraform_version}_linux_${arch_name}.zip" "terraform.zip"
 unzip -o -q terraform.zip -d /tmp
@@ -152,7 +145,7 @@ chmod +x /tmp/terraform
 mv /tmp/terraform /usr/local/bin
 rm -f terraform.zip
 
-# argocd
+# argocd-cli
 download "https://github.com/argoproj/argo-cd/releases/download/v${argocd_version}/argocd-linux-${arch_name}" "argocd"
 chmod +x ./argocd
 mv ./argocd /usr/local/bin/argocd
@@ -173,79 +166,3 @@ tar xfz fzf-${fzf_version}-linux_${arch_name}.tar.gz
 chmod +x fzf
 mv ./fzf /usr/local/bin
 rm -rf fzf-${fzf_version}-linux_${arch_name}.tar.gz
-cat << EOT >> ~/.bashrc
-
-# Set up fzf key bindings and fuzzy completion
-eval "\$(fzf --bash)"
-EOT
-
-
-REPOSITORY_OWNER="aws-samples"
-REPOSITORY_NAME="eks-workshop-v2"
-
-if [ ! -z "$REPOSITORY_REF" ]; then
-  cat << EOT > /usr/local/bin/reset-environment
-#!/bin/bash
-set -e
-curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/reset-environment | bash -s -- \$1
-EOT
-  chmod +x /usr/local/bin/reset-environment
-  cat << EOT > /usr/local/bin/delete-environment
-#!/bin/bash
-set -e
-curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/delete-environment | bash
-EOT
-  chmod +x /usr/local/bin/delete-environment
-  cat << EOT > /usr/local/bin/wait-for-lb
-#!/bin/bash
-set -e
-curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/wait-for-lb | bash -s -- \$1
-EOT
-  chmod +x /usr/local/bin/wait-for-lb
-  cat << EOT > /usr/local/bin/use-cluster
-#!/bin/bash
-set -e
-curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/use-cluster | bash -s -- \$1
-EOT
-  chmod +x /usr/local/bin/use-cluster
-  cat << EOT > /usr/local/bin/awshttp
-#!/bin/bash
-set -e
-curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/awshttp | bash -s -- \$1
-EOT
-  chmod +x /usr/local/bin/awshttp
-  cat << EOT > /usr/local/bin/delete-all-if-crd-exists
-#!/bin/bash
-set -e
-curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/delete-all-if-crd-exists | bash -s -- \$@
-EOT
-  chmod +x /usr/local/bin/delete-all-if-crd-exists
-  cat << EOT > /usr/local/bin/delete-all-and-wait-if-crd-exists
-#!/bin/bash
-set -e
-curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/delete-all-and-wait-if-crd-exists | bash -s -- \$@
-EOT
-  chmod +x /usr/local/bin/delete-all-and-wait-if-crd-exists
-  cat << EOT > /usr/local/bin/delete-nodegroup
-#!/bin/bash
-set -e
-curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/delete-nodegroup | bash -s -- \$1
-EOT
-  chmod +x /usr/local/bin/delete-nodegroup
-  cat << EOT > /usr/local/bin/uninstall-helm-chart
-#!/bin/bash
-set -e
-curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/uninstall-helm-chart | bash -s -- \$@
-EOT
-  chmod +x /usr/local/bin/uninstall-helm-chart
-  cat << EOT > /usr/local/bin/update-ide
-#!/bin/bash
-set -e
-curl -fsSL https://raw.githubusercontent.com/${REPOSITORY_OWNER}/${REPOSITORY_NAME}/$REPOSITORY_REF/lab/bin/update-ide | bash
-EOT
-  chmod +x /usr/local/bin/update-ide
-fi
-
-mkdir -p /eks-workshop
-
-chown ec2-user /eks-workshop
