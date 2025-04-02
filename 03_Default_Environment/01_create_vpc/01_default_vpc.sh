a21b0b5de72243a0e548e8fe58a7ac6fb0970b84
megazone-hcseo <hcseo@mz.co.kr>
2025-04-02 06:31:47 +0000

    feat: 3장 작업중

--- 03_Default_Environment/01_create_vpc/01_default_vpc.sh
+++ 03_Default_Environment/01_create_vpc/01_default_vpc.sh
@@ -9,11 +9,16 @@ fi
 STACK_NAME=eks-workshop-vpc-${IDE_NAME}
 # ==================================================================
 # Check if the stack already exists
-echo "스택 존재 여부 확인 중....."
+echo "${STACK_NAME} 스택 존재 여부 확인 중....."
+echo "aws cloudformation describe-stacks \\
+      --stack-name ${STACK_NAME} ${PROFILE_STRING}"
+
 if aws cloudformation describe-stacks --stack-name ${STACK_NAME} ${PROFILE_STRING} &> /dev/null; then
     echo "${STACK_NAME} 스택이 이미 존재합니다."
     exit 1
 fi
+echo "${STACK_NAME} 스택 존재 하지 않습니다."
+echo ""
 
 echo "aws cloudformation create-stack \\
   --stack-name ${STACK_NAME} \\
@@ -27,5 +32,5 @@ aws cloudformation create-stack \
   --capabilities CAPABILITY_NAMED_IAM ${PROFILE_STRING}
 
 aws cloudformation wait stack-create-complete \
-    --stack-name ${STACK_NAME} ${PROFILE_STRING}
+  --stack-name ${STACK_NAME} ${PROFILE_STRING}
 echo "기본 VPC 생성완료....."
