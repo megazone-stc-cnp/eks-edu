53b4e002136ca3919e5d8377eacf181b85ad003c
김종현 <yulisys@megazone.com>
2025-04-09 16:26:28 +0900

    update: 폴더명 단순화 및 스크립트 정리

--- 07_Addons_basic/01_coredns_addon/05_coredns_addon_create.sh
+++ 07_Addons_basic/01_coredns_addon/05_coredns_addon_create.sh
@@ -5,34 +5,28 @@ if [ -z "$1" ]; then
 fi
 ADDON_VERSION=$1
 
-if [ ! -f "../env.sh" ];then
+if [ ! -f "../../env.sh" ];then
 	echo "env.sh 파일 세팅을 해주세요."
 	exit 1
 fi
-. ../env.sh
+. ../../env.sh
 
+set -x
 ADDON_NAME=coredns
 # ============================================================
-
-echo "aws eks create-addon \\
-    --cluster-name ${CLUSTER_NAME} \\
-    --addon-name ${ADDON_NAME} \\
-    --addon-version ${ADDON_VERSION} \\
-    --resolve-conflicts OVERWRITE ${PROFILE_STRING}"
-
 aws eks create-addon \
     --cluster-name ${CLUSTER_NAME} \
     --addon-name ${ADDON_NAME} \
     --addon-version ${ADDON_VERSION} \
     --resolve-conflicts OVERWRITE ${PROFILE_STRING}
 
+set +x
 echo "COREDNS ADDON 생성중..."
-echo "aws eks wait addon-active \\
-    --cluster-name ${CLUSTER_NAME} \\
-    --addon-name ${ADDON_NAME} ${PROFILE_STRING}"
+set -x
 
 aws eks wait addon-active \
     --cluster-name ${CLUSTER_NAME} \
     --addon-name ${ADDON_NAME} ${PROFILE_STRING}
 
-echo "COREDNS ADDON 생성완료..."        
+set +x
+echo "COREDNS ADDON 생성완료..."
