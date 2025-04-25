cb098e736ee52d0aaafd9836ba2d72ed7e979e79
김종현 <yulisys@megazone.com>
2025-04-25 15:10:56 +0900

    네트워크 관리 내용 작성 중

--- 08_Network/02_get_vpc_cidr.sh
+++ 08_Network/02_get_vpc_cidr.sh
@@ -5,11 +5,11 @@ if [ ! -f "../env.sh" ];then
 fi
 . ../env.sh
 
-if [ ! -f "./local_env.sh" ];then
-	echo "local_env.sh 파일 세팅을 해주세요."
+if [ ! -f "../vpc_env.sh" ];then
+	echo "VPC를 먼저 생성해주세요. (vpc_env.sh 파일이 없습니다.)"
 	exit 1
 fi
-. ./local_env.sh
+. ../vpc_env.sh
 # export VPC_ID=vpc-045741095e8efe028
 # export AWS_AZ1=ap-northeast-1a
 # export AWS_AZ2=ap-northeast-1c
