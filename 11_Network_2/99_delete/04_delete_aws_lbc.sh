9dfcb7b2c8a03613e2543295feb78e783d59a14c
김종현 <yulisys@megazone.com>
2025-05-19 06:50:14 +0900

    update: aws-lbc

--- 11_Network_2/99_delete/04_delete_aws_lbc.sh
+++ 11_Network_2/99_delete/04_delete_aws_lbc.sh
@@ -10,6 +10,19 @@ export ADDON_IAM_POLICY_NAME=${CLUSTER_NAME}-addon-${ADDON_NAME}-iam-pol
 NAMESPACE_NAME=kube-system
 # =============================================================================
 
-aws iam list-policies --query "Policies[?PolicyName=='${ADDON_IAM_POLICY_NAME}'].Arn" --output text --no-cli-pager
+# NLB SG 삭제
+. ../02_aws_lbc_target_group_binding/local_env.sh
+aws ec2 delete-security-group --group-id ${NLB_SECURITY_GROUP_ID} --no-cli-pager
+# NLB Target Group 삭제
+aws elbv2 delete-target-group --target-group-arn ${TARGET_GROUP_ARN} --no-cli-pager
 
+# ALB SG 삭제
+. ../01_aws_lbc_ingress/local_env.sh
+aws ec2 delete-security-group --group-id ${ALB_SECURITY_GROUP_ID} --no-cli-pager
+
+# IAM Policy 삭제
+POLICY_ARN=$(aws iam list-policies --query "Policies[?PolicyName=='${ADDON_IAM_POLICY_NAME}'].Arn" --output text --no-cli-pager)
+aws iam delete-policy --policy-arn ${POLICY_ARN} --no-cli-pager
+
+# Helm Uninstall
 helm uninstall aws-load-balancer-controller -n ${NAMESPACE_NAME}
