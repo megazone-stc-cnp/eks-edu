e8c09b4ec663377897d1ec0e3f99b5f75aad40cf
서현철 <hcseo@mz.co.kr>
2025-06-19 11:34:02 +0900

    feat: ArgoCD 문서 작성

--- 14_ArgoCD/01_argo_install_http/01_argocd_repo_add.sh
+++ 14_ArgoCD/01_argo_install_http/01_argocd_repo_add.sh
@@ -3,7 +3,9 @@
 REPO_NAME=argo
 # ==============================================================
 echo "helm repo add ${REPO_NAME} https://argoproj.github.io/argo-helm"
+echo ""
 helm repo add ${REPO_NAME} https://argoproj.github.io/argo-helm
 
 echo "helm repo update"
+echo ""
 helm repo update
